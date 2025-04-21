cade1a107e65dc933e55412f1535e887ce4c72cb
seldomlabs <contact@seldomlabs.com>
2025-04-22 01:41:45 +0530

    added appHash in send otp api

--- apps/onboarding/src/onboarding.controller.ts
+++ apps/onboarding/src/onboarding.controller.ts
@@ -8,13 +8,21 @@ export class OnboardingController {
   constructor(private readonly onboardingService: OnboardingService) {}
 
   @Post('send-otp')
-  async sendOtp(@Body() data: { phoneNumber: string }, @Ip() ip: string) {
+  async sendOtp(@Body() data: { phoneNumber: string, appHash: string}, @Ip() ip: string) {
     try {
       if (!data.phoneNumber) {
         throw new HttpException({
           status: "ERROR",
           message: "Phone number is required",
           code: "MISSING_PHONE_NUMBER"
+        }, HttpStatus.BAD_REQUEST);
+      }
+
+      if (!data.appHash) {
+        throw new HttpException({
+          status: "ERROR",
+          message: "App hash is required",
+          code: "MISSING_APP_HASH"
         }, HttpStatus.BAD_REQUEST);
       }
 
